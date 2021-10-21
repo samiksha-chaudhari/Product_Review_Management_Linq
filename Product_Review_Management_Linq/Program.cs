b5a16259195c3084692e399d7aaadfbccefbf88a
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-21 23:19:54 +0530

    Welcome to Linq

---
+++ Product_Review_Management_Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Product_Review_Management_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management using Linq");
+        }
+    }
+}
